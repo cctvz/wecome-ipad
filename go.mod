3cf47fa92719ca1e646684a0646ce6faece66dd3
aiwzhonghua <962738330@qq.com>
2022-10-24 09:59:04 +0800

    获取二维码

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module wecome-ipad
 
 go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/spf13/viper v1.6.2
+
+)
--- main.go
+++ main.go
@@ -1,10 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+	"wecome-ipad/router"
 )
 
 func main() {
 
-	fmt.Println(111)
+	r := router.Router()
+
+	server := &http.Server{
+		Addr:              ":8080",
+		Handler:           r,
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		MaxHeaderBytes:    1 << 20,
+	}
+	log.Printf("[api] start http server listening %s", "8080")
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
