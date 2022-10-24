1ac58de89a72a47797c7637380ef26c4f69ae559
aiwzhonghua <962738330@qq.com>
2022-10-24 10:34:42 +0800

    获取二维码

--- controller/qrcode/qrcode.go
+++ controller/qrcode/qrcode.go
@@ -7,6 +7,10 @@ import (
 	pkg_api "wecome-ipad/util/http"
 )
 
+type qrcodeParams struct {
+	Token string `json:"token"`
+}
+
 type QrcodeGenerated struct {
 	Code string `json:"code"`
 	Data struct {
@@ -19,8 +23,15 @@ type QrcodeGenerated struct {
 
 func QrcodeController(c *gin.Context) {
 
+	var qrcodeParams qrcodeParams
+
+	if err := c.BindJSON(&qrcodeParams); err != nil {
+		c.JSON(http.StatusForbidden, "token miss")
+		return
+	}
+
 	apiHandler := pkg_api.MacApi{Authorization: c.GetHeader("Authorization")}
-	ret := apiHandler.GetJson("/login/qr_code", "Bearer skjfskldjfklsdjfklkdf")
+	ret := apiHandler.GetJson("/login/qr_code", qrcodeParams.Token)
 
 	resp := &QrcodeGenerated{}
 	_ = json.Unmarshal([]byte(ret), resp)
